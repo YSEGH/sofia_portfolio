fc9eaa59dd4c74bae39c8d95290fb9dd3a2dd3e5
youssef <youssef.segh@hotmail.fr>
2021-08-05 19:06:51 +0200

    heroku push

--- backend/server.js
+++ backend/server.js
@@ -28,4 +28,33 @@ const __dirname = path.resolve();
 
 app.use("/static-files", express.static(path.join(__dirname, "/static-files")));
 
-app.listen(3001, () => console.log("Server is ready !"));
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.use(
+  "/js",
+  (req, res, next) => {
+    req.url = req.url + ".gz";
+    res.setHeader("Content-Type", "application/json");
+    res.setHeader("Content-Encoding", "gzip");
+    res.setHeader("Content-Disposition", "gzip");
+    next();
+  },
+  express.static(path.join(__dirname, "/frontend/dist/js"))
+);
+app.use(
+  "/css",
+  (req, res, next) => {
+    req.url = req.url + ".gz";
+    res.setHeader("Content-Type", "text/css");
+    res.setHeader("Content-Encoding", "gzip");
+    res.setHeader("Content-Disposition", "gzip");
+    next();
+  },
+  express.static(path.join(__dirname, "/frontend/dist/css"))
+);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/frontend/dist/index.html"));
+});
+
+app.listen(process.env.PORT || 3001, () => console.log("Server is ready !"));
