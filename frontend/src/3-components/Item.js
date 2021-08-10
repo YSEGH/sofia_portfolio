103b0d6cbac383ae198f48e3b9e3d8a3a79a6dbf
youssef <youssef.segh@hotmail.fr>
2021-08-10 11:46:09 +0200

    push heroku

--- frontend/src/3-components/Item.js
+++ frontend/src/3-components/Item.js
@@ -1,14 +1,8 @@
 import React, { useEffect } from "react";
 import "../1-css/Item.css";
-import { MdPlace, MdDateRange, MdWork } from "react-icons/md";
-import { IoEarthOutline } from "react-icons/io5";
 import { Link } from "react-router-dom";
 
 export default function Item({ item }) {
-  /*   const image = item.photos.filter(
-    (photo) => photo.type.split("/")[0] !== "video"
-  ); */
-
   useEffect(() => {
     return () => {};
   }, []);
