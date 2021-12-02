5a6d6b0e561c62585c578de01eb504f290fe2eb6
Youssef <youssef.segh@hotmail.fr>
2021-12-02 13:18:49 +0100

    home page refonte

--- frontend/src/2-pages/Home.js
+++ frontend/src/2-pages/Home.js
@@ -8,22 +8,20 @@ export default function Home() {
 
     return () => {};
   }, []);
-
   return (
     <div className="home">
-      <div className="photo-container">
-        <img src="/static-files/archi1.jpg" />
-      </div>
       <div className="text-container">
         <h1>
           <span>Sofia</span>
-          <br />
           Seghrouchni
         </h1>
         <p>
           Architecture
           <span> & Paysagisme</span>
         </p>
+      </div>
+      <div className="photo-container">
+        <img src="/static-files/skyline_marseille_lg.png" />
       </div>
     </div>
   );
